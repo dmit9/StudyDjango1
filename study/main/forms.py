f599b52a60709c533dbfcff85ed919912540c2a4
dmit9 <devyatin5@gmail.com>
2022-04-27 19:11:40 +0300

    lesson end

--- study/main/forms.py
+++ study/main/forms.py
@@ -1,9 +1,19 @@
-﻿from dataclasses import fields
+﻿##from dataclasses import fields
 from .models import TaskTable
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 
 
 class TaskTableForm(ModelForm):
     class Meta:
         model = TaskTable
         fields = ['title', 'task']
+        widgets = {
+        "title": TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ввести кв'
+        }),
+        "task": Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ввести месяц'
+        }),
+            }
--- study/main/views.py
+++ study/main/views.py
@@ -1,5 +1,5 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 #from django.http import HttpResponse
 from .models import TaskTable
 from .forms import TaskTableForm
@@ -13,8 +13,18 @@ def about(reguest):
     return render(reguest, "main/about.html")
 
 def create(reguest):
+    error = ''
+    if reguest.method == 'POST':
+        form = TaskTableForm(reguest.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма неверна!'
+
     form = TaskTableForm()
     context = {
-        'form': form
+        'form': form,
+        'error': error
     }
-    return render(reguest, "main/create.html")
+    return render(reguest, "main/create.html", context)
